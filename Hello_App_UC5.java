2829e4cfb2ca0f92cbdeeb7adee791ef11fd4926
DharshiniSuresh07 <sdharshini1310@gmail.com>
2026-04-05 11:38:57 +0530

    Coded UC5: Display Hello with Multiple Names using StringBuilder

---
+++ Hello_App_UC5.java
@@ -0,0 +1,23 @@
+public class Hello_App_UC5 {
+
+    public static void main(String[] args) {
+
+        StringBuilder names = new StringBuilder();
+
+        if (args.length == 0) {
+            names.append("World");
+        } else {
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names.append(", ");
+                }
+                names.append(name);
+                first = false;
+            }
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
